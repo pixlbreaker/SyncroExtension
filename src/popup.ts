bf2b67110fa83edf773281ebeb30781ca5381389
Michael Skotar <michaelskotar@gmail.com>
2025-05-01 23:53:58 -0400

    Working on changing it to typescript

---
+++ src/popup.ts
@@ -0,0 +1,6 @@
+const roomInput = document.getElementById("room") as HTMLInputElement;
+const joinButton = document.getElementById("join") as HTMLButtonElement;
+
+joinButton.addEventListener("click", () => {
+    console.log("Clicked");
+});
