ad7b6533e7d0fb667ed8477917052fff363456b3
Michael Skotar <michaelskotar@gmail.com>
2025-05-01 23:56:06 -0400

    Changed back to vanilla js

---
+++ src/popup.js
@@ -0,0 +1,6 @@
+const roomInput = document.getElementById("room");
+const joinButton = document.getElementById("join");
+
+joinButton.addEventListener("click", () => {
+    console.log("Clicked");
+});
--- src/popup.ts
+++
@@ -1,6 +0,0 @@
-const roomInput = document.getElementById("room") as HTMLInputElement;
-const joinButton = document.getElementById("join") as HTMLButtonElement;
-
-joinButton.addEventListener("click", () => {
-    console.log("Clicked");
-});
