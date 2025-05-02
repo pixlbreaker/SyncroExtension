0407c4c4ed21b603052283f5fe1039b985635b9d
Michael Skotar <michaelskotar@gmail.com>
2025-05-02 17:37:15 -0400

    Add a firebase config file

---
+++ src/firebase-config.ts
@@ -0,0 +1,10 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+
+const firebaseConfig = {
+
+}
+
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
